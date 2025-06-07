56725e14ac180f0a8c612650452a06b6fd0abeb1
gtalpo <gtalpo@gmail.com>
2025-06-08 08:48:25 +0900

    필드 데이터 작성

--- ch2_game_of_life/src/main.rs
+++ ch2_game_of_life/src/main.rs
@@ -1,3 +1,25 @@
+const FieldWidth: usize = 12;
+const FieldHeight: usize = 12;
+
+#[derive(Debug)]
+struct Context {
+    field: Vec<bool>, //bool[FieldHeight][FieldWidth];
+}
+
+impl Context {
+    pub fn new() -> Self {
+        let mut field = vec![false; FieldHeight * FieldWidth];
+        field[0 * FieldWidth + 1] = true;
+        field[1 * FieldWidth + 2] = true;
+        field[2 * FieldWidth + 0] = true;
+        field[2 * FieldWidth + 1] = true;
+        field[2 * FieldWidth + 2] = true;
+        Self { field }
+    }
+}
+
 fn main() {
-    loop {}
+    // loop {}
+    let mut context = Context::new();
+    println!("{:?}", context);
 }
