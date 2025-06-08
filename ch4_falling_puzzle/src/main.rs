fcbb551dd6bc29492c2a1d030c8004879ae641d3
gtalpo <gtalpo@gmail.com>
2025-06-08 21:59:00 +0900

    화면 그리는 함수

--- ch4_falling_puzzle/src/main.rs
+++ ch4_falling_puzzle/src/main.rs
@@ -2,7 +2,10 @@ struct Context {}
 
 impl Context {
     pub fn new() -> Self {}
-    pub fn init(&mut self) {}
+    pub fn init(&mut self) {
+        self.draw_screen();
+    }
+    pub fn draw_screen(&self) {}
 }
 
 fn main() {
