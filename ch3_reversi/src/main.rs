a6e6b6611530daca6924907403f9a47c39b17626
gtalpo <gtalpo@gmail.com>
2025-06-08 14:40:52 +0900

    커서 조작

--- ch3_reversi/src/main.rs
+++ ch3_reversi/src/main.rs
@@ -1,3 +1,5 @@
+use getch_rs::{Getch, Key};
+
 const BOARD_WIDTH: usize = 8;
 const BOARD_HEIGHT: usize = 8;
 
@@ -9,16 +11,17 @@ enum TurnEnum {
     Max = 3,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Default)]
 struct Vec2 {
-    x: usize,
-    y: usize,
+    x: i8,
+    y: i8,
 }
 
 struct Context {
     board: Vec<TurnEnum>,
     disk_aa: [String; TurnEnum::Max as usize],
     cursor_position: Vec2,
+    g: Getch,
 }
 
 impl Context {
@@ -26,7 +29,8 @@ impl Context {
         Self {
             board: vec![TurnEnum::None; BOARD_HEIGHT * BOARD_WIDTH],
             disk_aa: ["●".to_string(), "○".to_string(), "·".to_string()],
-            cursor_position: Vec2 { x: 0, y: 0 },
+            cursor_position: Vec2::default(),
+            g: Getch::new(),
         }
     }
     pub fn init(&mut self) {
@@ -37,17 +41,19 @@ impl Context {
         self.draw_screen();
     }
     pub fn draw_screen(&self) {
+        clearscreen::clear().unwrap();
+
         for y in 0..BOARD_HEIGHT {
             for x in 0..BOARD_WIDTH {
                 print!("{}", self.disk_aa[self.board[y * BOARD_WIDTH + x] as usize]);
             }
-            if y == self.cursor_position.y {
+            if y == self.cursor_position.y as usize {
                 print!("←");
             }
             println!();
         }
         for x in 0..BOARD_WIDTH {
-            if x == self.cursor_position.x {
+            if x == self.cursor_position.x as usize {
                 print!("↑");
             } else {
                 print!(" ");
@@ -55,10 +61,43 @@ impl Context {
         }
         println!();
     }
+    pub fn input_position(&mut self) -> Vec2 {
+        loop {
+            self.draw_screen();
+
+            match self.g.getch() {
+                Ok(Key::Char('w')) => {
+                    self.cursor_position.y -= 1;
+                }
+                Ok(Key::Char('s')) => {
+                    self.cursor_position.y += 1;
+                }
+                Ok(Key::Char('a')) => {
+                    self.cursor_position.x -= 1;
+                }
+                Ok(Key::Char('d')) => {
+                    self.cursor_position.x += 1;
+                }
+                Ok(Key::Esc) => {
+                    std::process::exit(0);
+                }
+                _ => {}
+            }
+            self.cursor_position.x =
+                (BOARD_WIDTH as i8 + self.cursor_position.x) % (BOARD_WIDTH as i8);
+            self.cursor_position.y =
+                (BOARD_HEIGHT as i8 + self.cursor_position.y) % (BOARD_HEIGHT as i8);
+        }
+        return Vec2::default();
+    }
 }
 
 fn main() {
     let mut ctx = Context::new();
     ctx.init();
-    // loop {}
+    loop {
+        let mut place_position = Vec2::default();
+
+        place_position = ctx.input_position();
+    }
 }
