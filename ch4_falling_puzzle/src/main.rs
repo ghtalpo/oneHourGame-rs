b8024d5fb6c1093c5327020bcc954c0388b1a52f
gtalpo <gtalpo@gmail.com>
2025-06-08 22:06:06 +0900

    필드 데이터 작성

--- ch4_falling_puzzle/src/main.rs
+++ ch4_falling_puzzle/src/main.rs
@@ -1,7 +1,34 @@
-struct Context {}
+const FIELD_WIDTH: usize = 12;
+const FIELD_HEIGHT: usize = 18;
+
+enum BlockEnum {
+    None = 0,
+    Hard = 1,
+    Max = 2,
+}
+
+struct Context {
+    field: [u8; FIELD_HEIGHT * FIELD_WIDTH],
+    default_field: [u8; FIELD_HEIGHT * FIELD_WIDTH],
+}
 
 impl Context {
-    pub fn new() -> Self {}
+    pub fn new() -> Self {
+        let default_field = [
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0,
+            0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        ];
+        Self {
+            field: [0; FIELD_HEIGHT * FIELD_WIDTH],
+            default_field,
+        }
+    }
     pub fn init(&mut self) {
         self.draw_screen();
     }
@@ -11,5 +38,5 @@ impl Context {
 fn main() {
     let mut ctx = Context::new();
     ctx.init();
-    loop {}
+    // loop {}
 }
