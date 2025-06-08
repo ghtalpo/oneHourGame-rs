11c6f533ce562a23f93be760acc5754907323f33
gtalpo <gtalpo@gmail.com>
2025-06-08 14:25:55 +0900

    커서 그리기

--- ch3_reversi/src/main.rs
+++ ch3_reversi/src/main.rs
@@ -9,9 +9,16 @@ enum TurnEnum {
     Max = 3,
 }
 
+#[derive(Clone, Copy)]
+struct Vec2 {
+    x: usize,
+    y: usize,
+}
+
 struct Context {
     board: Vec<TurnEnum>,
     disk_aa: [String; TurnEnum::Max as usize],
+    cursor_position: Vec2,
 }
 
 impl Context {
@@ -19,6 +26,7 @@ impl Context {
         Self {
             board: vec![TurnEnum::None; BOARD_HEIGHT * BOARD_WIDTH],
             disk_aa: ["●".to_string(), "○".to_string(), "·".to_string()],
+            cursor_position: Vec2 { x: 0, y: 0 },
         }
     }
     pub fn init(&mut self) {
@@ -31,15 +39,24 @@ impl Context {
     pub fn draw_screen(&self) {
         for y in 0..BOARD_HEIGHT {
             for x in 0..BOARD_WIDTH {
-                print!(
-                    "{:2}",
-                    self.disk_aa[self.board[y * BOARD_WIDTH + x] as usize]
-                );
+                print!("{}", self.disk_aa[self.board[y * BOARD_WIDTH + x] as usize]);
+            }
+            if y == self.cursor_position.y {
+                print!("←");
             }
             println!();
         }
+        for x in 0..BOARD_WIDTH {
+            if x == self.cursor_position.x {
+                print!("↑");
+            } else {
+                print!(" ");
+            }
+        }
+        println!();
     }
 }
+
 fn main() {
     let mut ctx = Context::new();
     ctx.init();
