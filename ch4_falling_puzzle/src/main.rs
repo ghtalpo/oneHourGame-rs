d3c6d14dd0a10d85145b60444e65e8b0bf032145
gtalpo <gtalpo@gmail.com>
2025-06-08 21:58:43 +0900

    초기화 함수

--- ch4_falling_puzzle/src/main.rs
+++ ch4_falling_puzzle/src/main.rs
@@ -1,3 +1,12 @@
+struct Context {}
+
+impl Context {
+    pub fn new() -> Self {}
+    pub fn init(&mut self) {}
+}
+
 fn main() {
+    let mut ctx = Context::new();
+    ctx.init();
     loop {}
 }
