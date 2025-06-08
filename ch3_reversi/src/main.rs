183bd495a27a3c271ff497954fbdd03a11d30037
gtalpo <gtalpo@gmail.com>
2025-06-08 13:13:00 +0900

    모눈판 초기화

--- ch3_reversi/src/main.rs
+++ ch3_reversi/src/main.rs
@@ -1,3 +1,27 @@
+const BOARD_WIDTH: usize = 8;
+const BOARD_HEIGHT: usize = 8;
+
+enum TurnEnum {
+    Black = 0,
+    White = 1,
+    None = 2,
+    Max = 3,
+}
+
+struct Context {
+    board: vec<TurnEnum>,
+}
+
+impl Context {
+    pub fn new() -> Self {
+        Self {
+            board: vec![TurnEnum.None; BOARD_HEIGHT * BOARD_WIDTH],
+        }
+    }
+    pub fn init(&mut self) {}
+}
 fn main() {
+    let mut ctx = Context::new();
+    ctx.init();
     loop {}
 }
