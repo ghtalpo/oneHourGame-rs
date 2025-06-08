76b7d0d7b6dd0dfd1ce8a6edc847c134c33dcaa6
gtalpo <gtalpo@gmail.com>
2025-06-08 14:20:47 +0900

    모눈판 초기 배치

--- ch3_reversi/src/main.rs
+++ ch3_reversi/src/main.rs
@@ -18,16 +18,23 @@ impl Context {
     pub fn new() -> Self {
         Self {
             board: vec![TurnEnum::None; BOARD_HEIGHT * BOARD_WIDTH],
-            disk_aa: ["⬤".to_string(), "◯".to_string(), "·".to_string()],
+            disk_aa: ["●".to_string(), "○".to_string(), "·".to_string()],
         }
     }
     pub fn init(&mut self) {
+        self.board[4 * BOARD_WIDTH + 3] = TurnEnum::Black;
+        self.board[3 * BOARD_WIDTH + 4] = TurnEnum::Black;
+        self.board[3 * BOARD_WIDTH + 3] = TurnEnum::White;
+        self.board[4 * BOARD_WIDTH + 4] = TurnEnum::White;
         self.draw_screen();
     }
     pub fn draw_screen(&self) {
         for y in 0..BOARD_HEIGHT {
             for x in 0..BOARD_WIDTH {
-                print!("{}", self.disk_aa[self.board[y * BOARD_WIDTH + x] as usize]);
+                print!(
+                    "{:2}",
+                    self.disk_aa[self.board[y * BOARD_WIDTH + x] as usize]
+                );
             }
             println!();
         }
