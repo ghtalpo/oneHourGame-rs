dbabd701808ca6058a21b2889b705f3c65c2708f
gtalpo <gtalpo@gmail.com>
2025-06-12 21:57:41 +0900

    미로 데이터 만들기

--- ch6_maze/src/main.rs
+++ ch6_maze/src/main.rs
@@ -1,2 +1,39 @@
+const MAZE_WIDTH: usize = 8;
+const MAZE_HEIGHT: usize = 8;
+
+enum DirectionEnum {
+    North = 0,
+    West,
+    South,
+    East,
+    Max,
+}
+
+#[derive(Clone, Copy)]
+struct Tile {
+    wall: [bool; DirectionEnum::Max as usize],
+}
+
+impl Tile {
+    pub fn new() -> Self {
+        Self {
+            wall: [false; DirectionEnum::Max as usize],
+        }
+    }
+}
+
+struct Context {
+    maze: [Tile; MAZE_HEIGHT * MAZE_WIDTH],
+}
+
+impl Context {
+    pub fn new() -> Self {
+        Self {
+            maze: [Tile::new(); MAZE_HEIGHT * MAZE_WIDTH],
+        }
+    }
+}
+
 fn main() {
+    loop {}
 }
