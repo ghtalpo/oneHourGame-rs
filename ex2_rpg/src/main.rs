e3bbe60b0e4a5493c60a3a64213d34f210814763
gtalpo <gtalpo@gmail.com>
2025-06-15 15:53:24 +0900

    소스 정리

--- ex2_rpg/src/main.rs
+++ ex2_rpg/src/main.rs
@@ -3,7 +3,6 @@ use std::convert::TryInto;
 
 use getch_rs::Getch;
 use getch_rs::Key;
-// use rand::random_range;
 use rand::Rng;
 use rand::rngs::ThreadRng;
 
@@ -66,6 +65,7 @@ impl CommandEnum {
     }
 }
 
+// [3-4]맵의 종류를 정의한다
 #[derive(Copy, Clone, PartialEq)]
 enum MapEnum {
     Field = 0,
@@ -75,7 +75,6 @@ enum MapEnum {
 }
 
 // [4-1]캐릭터의 구조체를 선언한다
-
 #[derive(Clone)]
 struct Character {
     hp: i64,
@@ -118,9 +117,11 @@ struct Context {
 
 impl Context {
     pub fn new() -> Self {
+        // [5-4]맵의 지형 데이터를 선언한다
         let mut map = [0_u8; MapEnum::Max as usize * MAP_HEIGHT * MAP_WIDTH];
 
         for (i, v) in vec![
+            // [5-4-1]MAP_FIELD 필드
             "~~~~~~~~~~~~~~~~",
             "~~MMMMM~~MMMM.~~",
             "~M...M.##..M...~",
@@ -137,6 +138,7 @@ impl Context {
             "~......~~M.MM..~",
             "~~....~~~~....~~",
             "~~~~~~~~~~~~~~~~",
+            // [5-4-2]MAP_KING_CASTLE   왕의 성
             "HHH.......HHH\0\0\0",
             "H.H.......H.H\0\0\0",
             "HHHHHHHHHHHHH\0\0\0",
@@ -153,6 +155,7 @@ impl Context {
             "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
             "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
             "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
+            // [5-4-3]MAP_BOSS_CASTLE   마왕의 성
             "HHH.......HHH\0\0\0",
             "H.H.......H.H\0\0\0",
             "HHHHHHHHHHHHH\0\0\0",
@@ -217,9 +220,12 @@ impl Context {
                     ..Character::default()
                 },
             ],
+            // [5-2]캐릭터의 배열을 선언한다
             characters: [Character::default(), Character::default()],
             map,
+            // [6-1-2]현재 맵을 초기화한다
             current_map: MapEnum::KingCastle,
+            // [6-1-3]플레이어의 좌표를 초기화한다
             player_x: 4,
             player_y: 6,
             rng: rand::rng(),
@@ -232,6 +238,12 @@ impl Context {
         // [6-1-1]플레이어의 상태를 초기화한다
         self.characters[CharacterEnum::Player as usize] =
             self.monsters[MonsterEnum::Player as usize].clone();
+
+        // [6-1-2]현재 맵을 초기화한다
+        self.current_map = MapEnum::KingCastle;
+
+        self.player_x = 4; // [6-1-3]플레이어의 X좌표를 초기화한다
+        self.player_y = 6; // [6-1-4]플레이어의 Y좌표를 초기화한다
     }
 
     // [6-2]전투 장면의 화면을 그리는 함수를 선언한다
@@ -268,7 +280,8 @@ impl Context {
 
     // [6-3]명령을 선택하는 함수를 선언한다
     fn select_command(&mut self) {
-        let command_names = ["싸운다", "주문", "도망친다"];
+        // [5-3]명령의 이름을 선언한다
+        const COMMAND_NAMES: [&'static str; 3] = ["싸운다", "주문", "도망친다"];
 
         // [6-3-1]플레이어의 명령을 초기화한다
         self.characters[CharacterEnum::Player as usize].command = CommandEnum::Fight;
@@ -277,7 +290,7 @@ impl Context {
             self.draw_battle_screen();
 
             // [6-3-4]명령 목록을 표시한다
-            for (i, command_name) in command_names.iter().enumerate() {
+            for (i, command_name) in COMMAND_NAMES.iter().enumerate() {
                 if i == self.characters[CharacterEnum::Player as usize].command as usize {
                     print!(">");
                 } else {
@@ -298,6 +311,7 @@ impl Context {
                         .command
                         .increase();
                 }
+                Ok(Key::Esc) => std::process::exit(0),
                 _ => {
                     return;
                 }
@@ -437,7 +451,9 @@ impl Context {
         self.map[map as usize * (MAP_HEIGHT * MAP_WIDTH) + y * MAP_WIDTH + x]
     }
 
+    // [6-5]맵을 그리는 처리를 기술하는 함수를 선언한다
     fn draw_map(&self) {
+        // [6-5-1]화면을 클리어한다
         clearscreen::clear().unwrap();
 
         let y0 = self.player_y as isize - SCREEN_HEIGHT as isize / 2;
@@ -446,7 +462,9 @@ impl Context {
         let x1 = self.player_x as isize + SCREEN_WIDTH as isize / 2;
         for y in y0..y1 {
             for x in x0..x1 {
+                // [6-5-4]대상 좌표가 플레이어의 좌표와 같은지 여부를 판정한다
                 if x == self.player_x as isize && y == self.player_y as isize {
+                    // [6-5-5]플레이어의 아스키아트를 그린다
                     print!("勇");
                 } else if x < 0
                     || x >= MAP_WIDTH as isize
@@ -454,26 +472,30 @@ impl Context {
                     || y >= MAP_HEIGHT as isize
                     || self.get_cell_xy(self.current_map, x as usize, y as usize) == 0
                 {
+                    // [6-5-6]대상 좌표가 맵 데이터의 범위인지 여부를 판정한다
+
                     match self.current_map {
-                        MapEnum::Field => print!("~~"),
-                        MapEnum::KingCastle => print!(". "),
-                        MapEnum::BossCastle => print!(". "),
+                        MapEnum::Field => print!("~~"),      // [6-5-8]필드 밖은 바다
+                        MapEnum::KingCastle => print!(". "), // [6-5-9]왕의 성 밖은 평지
+                        MapEnum::BossCastle => print!(". "), // [6-5-10]마왕의 성 밖은 평지
                         _ => {}
                     }
                 } else {
+                    // [6-5-11]상기의 상태가 아니면
+
                     match self.get_cell_xy(self.current_map, x as usize, y as usize) as char {
-                        '~' => print!("~~"),
-                        '.' => print!(". "),
-                        'M' => print!("MM"),
-                        '#' => print!("##"),
-                        'K' => print!("王"),
-                        'B' => print!("魔"),
-                        'H' => print!("□□"),
-                        'W' => print!("炎"),
-                        'Y' => print!("Y "),
-                        '0' => print!("王"),
-                        '1' => print!("姫"),
-                        '2' => print!("魔"),
+                        '~' => print!("~~"), // [6-5-13]바다
+                        '.' => print!(". "), // [6-5-14]평지
+                        'M' => print!("MM"), // [6-5-15]산
+                        '#' => print!("##"), // [6-5-16]다리
+                        'K' => print!("王"), // [6-5-17]왕의 성
+                        'B' => print!("魔"), // [6-5-18]마왕의 성
+                        'H' => print!("□□"), // [6-5-19]벽
+                        'W' => print!("炎"), // [6-5-20]불
+                        'Y' => print!("Y "), // [6-5-21]촛대
+                        '0' => print!("王"), // [6-5-22]왕
+                        '1' => print!("姫"), // [6-5-23]공주님
+                        '2' => print!("魔"), // [6-5-24]마왕
                         _ => {}
                     }
                 }
@@ -482,8 +504,10 @@ impl Context {
         }
         println!();
 
+        // [6-5-27]플레이어의 이름을 표시한다
         println!("{}", self.characters[CharacterEnum::Player as usize].name);
 
+        // [6-5-28]플레이어의 상태를 표시한다
         println!(
             "HP:{}/{} MP:{}/{}",
             self.characters[CharacterEnum::Player as usize].hp,
@@ -509,13 +533,15 @@ fn main() {
     ctx.init();
 
     // [6-6-3]전투 장면의 함수를 호출한다
-    // ctx.battle(MonsterEnum::Boss);
     loop {
+        // [6-6-4]맵을 그리는 함수를 호출한다
         ctx.draw_map();
 
+        // [6-6-5]플레이어의 이동 전 좌표를 선언한다
         let last_player_x = ctx.player_x;
         let last_player_y = ctx.player_y;
 
+        // [6-6-7]입력된 키로 분기한다
         match ctx.g.getch() {
             Ok(Key::Char('w')) => ctx.player_y -= 1,
             Ok(Key::Char('s')) => ctx.player_y += 1,
@@ -525,6 +551,7 @@ fn main() {
             _ => {}
         }
 
+        // [6-6-12]맵의 밖으로 나갔는지 여부를 판정한다
         if ctx.player_x < 0
             || ctx.player_x >= MAP_WIDTH
             || ctx.player_y < 0
@@ -535,14 +562,21 @@ fn main() {
                 ctx.player_y as usize,
             ) == 0
         {
+            // [6-6-13]현재 맵에 따라 분기한다
             match ctx.current_map {
                 MapEnum::KingCastle => {
+                    // [6-6-14]왕의 성
+
+                    // [6-6-15]필드 맵으로 전환한다
                     ctx.current_map = MapEnum::Field;
 
                     ctx.player_x = 6;
                     ctx.player_y = 12;
                 }
                 MapEnum::BossCastle => {
+                    // [6-6-18]마왕의 성
+
+                    // [6-6-19]필드 맵으로 전환한다
                     ctx.current_map = MapEnum::Field;
 
                     ctx.player_x = 10;
@@ -552,25 +586,21 @@ fn main() {
             }
         }
 
+        // [6-6-22]이동 목적지 칸의 종류에 따라 분기시킨다
         match ctx.get_cell_xy(ctx.current_map, ctx.player_x, ctx.player_y) as char {
-            '.' | '#' => {
-                if ctx.current_map == MapEnum::Field && ctx.rng.random_range(0..16) == 0 {
-                    ctx.battle(MonsterEnum::Slime);
-                }
-            }
             'K' => {
+                // [6-6-23]왕의 성
+
+                // [6-6-24]왕의 성으로 맵을 전환한다
                 ctx.current_map = MapEnum::KingCastle;
 
                 ctx.player_x = 6;
                 ctx.player_y = 12;
             }
-            'B' => {
-                ctx.current_map = MapEnum::BossCastle;
-
-                ctx.player_x = 6;
-                ctx.player_y = 15;
-            }
             '0' => {
+                // [6-6-31]왕
+
+                // [6-6-32]왕의 대화 메시지를 표시한다
                 println!(
                     "*[용사여!\n\
                     동쪽 마성의 마왕을\n\
@@ -580,6 +610,9 @@ fn main() {
                 ctx.pause_a_key();
             }
             '1' => {
+                // [6-6-34]공주
+
+                // [6-6-35]공주의 대화 메시지를 표시한다
                 println!(
                     "*[신께 기도드립니다.!\n\
                     오, 신이시여!\n\
@@ -588,13 +621,27 @@ fn main() {
 
                 ctx.pause_a_key();
 
+                // [6-6-37]플레이어의 HP를 회복시킨다
                 ctx.characters[CharacterEnum::Player as usize].hp =
                     ctx.characters[CharacterEnum::Player as usize].max_hp;
 
+                // [6-6-38]플레이어의 MP를 회복시킨다
                 ctx.characters[CharacterEnum::Player as usize].mp =
                     ctx.characters[CharacterEnum::Player as usize].max_mp;
             }
+            'B' => {
+                // [6-6-27]마왕의 성
+
+                // [6-6-28]마왕의 성으로 맵을 전환한다
+                ctx.current_map = MapEnum::BossCastle;
+
+                ctx.player_x = 6;
+                ctx.player_y = 15;
+            }
             '2' => {
+                // [6-6-39]마왕
+
+                // [6-6-40]마왕의 대화 메시지를 표시한다
                 println!(
                     "*어리석은 인간이여!\n\
                     나의 야망을 방해하는 자는\n\
@@ -603,11 +650,14 @@ fn main() {
 
                 ctx.pause_a_key();
 
+                // [6-6-42]마왕과의 전투를 발생시킨다
                 ctx.battle(MonsterEnum::Boss);
 
+                // [6-6-43]마왕이 죽었는지 여부를 판정한다
                 if ctx.characters[CharacterEnum::Monster as usize].hp <= 0 {
                     clearscreen::clear().unwrap();
 
+                    // [6-6-45]엔딩 메시지를 표시한다
                     println!(
                         "  마왕은 사라지고 세상은\n\
                         멸망의 위기에서 구원되다!\n\
@@ -625,17 +675,40 @@ fn main() {
                     std::process::exit(0);
                 }
             }
+
+            _ => {}
+        }
+
+        // [6-6-48]이동 목적지 칸의 종류에 따라 분기시킨다
+        match ctx.get_cell_xy(ctx.current_map, ctx.player_x, ctx.player_y) as char {
+            '.' | '#' => {
+                // [6-6-49]평지
+                // [6-6-50]다리
+
+                // [6-6-51]적과 조우했는지 여부를 판정한다
+                if ctx.current_map == MapEnum::Field && ctx.rng.random_range(0..16) == 0 {
+                    // [6-6-52]잡어 몬스터와의 전투를 발생시킨다
+                    ctx.battle(MonsterEnum::Slime);
+                }
+            }
             _ => {
+                // [6-6-53]상기 이외의 칸
+
+                // [6-6-54]플레이어의 좌표를 이동 전으로 되돌린다
                 ctx.player_x = last_player_x;
                 ctx.player_y = last_player_y;
             }
         }
 
+        // [6-6-56]플레이어가 사망했는지 여부를 판정한다
         if ctx.characters[CharacterEnum::Player as usize].hp <= 0 {
+            // [6-6-57]게임을 초기화하는 함수를 호출한다
             ctx.init();
 
+            // [6-6-58]화면을 다시 그린다
             ctx.draw_map();
 
+            // [6-6-59]왕의 메시지를 표시한다
             println!(
                 "* 오 용사여!\n\
                 신이 그대를 구원하셨다!\n\
